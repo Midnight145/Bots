70ff48a4e187fc33e4d4e6de9a1d58f1675bc669
shadowlynch <shadowlynch101@gmail.com>
2020-05-15 17:48:17 -0500

    rough framework for levelup bot

--- Levelupbot.py
+++ Levelupbot.py
@@ -1,12 +1,32 @@
 from getpass import getpass
+from discord import Message
+from json import dumps, loads
 from discord.ext.commands import Bot, Context
 
 client = Bot(command_prefix="!")
 
+exp_dictionary = {}
+level_dictionary = {}
 
-@client.command()
-async def testCommand(ctx: Context, *, question: str):
-    await ctx.send(question)
+
+@client.event
+async def on_message(message: Message):
+    if message.author != client.user:
+        context = await client.get_context(message)
+
+        if message.author in exp_dictionary.keys():
+            exp_dictionary[message.author] += 10
+        else:
+            exp_dictionary[message.author] = 10
+
+        for user in exp_dictionary:
+            if exp_dictionary[user] >= 100:
+                if user in level_dictionary.keys():
+                    level_dictionary[user] += 1
+                else:
+                    level_dictionary[user] = 1
+
+                await context.send("You leveled up!")
 
 
 client.run(getpass("What is your discord secret: "))
